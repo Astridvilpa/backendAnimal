196967cec6690bf096e8630eb323f23ed3042922
Astrid Vilora <astridviloria54@gmail.com>
2024-07-22 19:08:45 +0200

    Se crea la migración de appointment

---
+++ src/database/migrations/20240722170309-create-appointment.js
@@ -0,0 +1,54 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("appointments", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      Service_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "services",
+          key: "id",
+        },
+      },
+      Pet_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "pets",
+          key: "id",
+        },
+      },
+      Veterinario_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "veterinarios",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("appointments");
+  },
+};
